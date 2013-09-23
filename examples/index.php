01f566cd01dcf37aea5ce9c0d11d2c01293e5b69
Svpernova09 <halo09@gmail.com>
2013-09-23 18:26:56 -0500

    Stripping out composer and bootstrap. Overcomplicating the code

--- examples/index.php
+++ examples/index.php
@@ -1,13 +1,22 @@
 <?php
 $folders = array_filter(glob('*'), 'is_dir');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Getting Started with PHP</title>
+</head>
+<body>
+<h1>Getting Started with PHP!</h1>
 <ul>
     <?php
     foreach($folders as $folder){
         $title = str_replace('-',' ',$folder);
         ?>
         <li><a href="<?php echo $folder; ?>"><?php echo $title; ?></a></li>
-        <?php
+    <?php
     }
     ?>
 </ul>
+</body>
+</html>
